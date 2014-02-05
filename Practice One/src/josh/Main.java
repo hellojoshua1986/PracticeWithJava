16c842cef284cc1a9b3b5b30e133129ebcc56ea3
Thanh Joshua Duong <tjoshuaduong@gmail.com>
2014-02-04 16:15:08 -0800

    Practice!

---
+++ Practice One/src/josh/Main.java
@@ -0,0 +1,20 @@
+/** 
+ * 
+ */
+package josh;
+
+/**
+ * @author Josh Duong
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("hola");
+	}
+
+}
